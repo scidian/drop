9edd804c756aa984143fb417032f21bd57168eb0
Stephens Nunnally <stevinz@gmail.com>
2019-01-04 00:20:13 -0500

    Fixed multi selected scaling

--- editor_scene_view.cpp
+++ editor_scene_view.cpp
@@ -85,7 +85,7 @@ void SceneGraphicsView::mousePressEvent(QMouseEvent *event)
             // If left down, store item that was under mouse
             m_origin_item = itemAt(event->pos());
 
-            // If no keys are down, only select item under mouse
+            // ******************* If no keys are down, only select item under mouse
             if (event->modifiers() == Qt::KeyboardModifier::NoModifier) {
                 // If no item under mouse, deselect all
                 if (m_origin_item == nullptr) {
@@ -105,7 +105,7 @@ void SceneGraphicsView::mousePressEvent(QMouseEvent *event)
                 if (m_origin_item != nullptr) m_origin_item->setSelected(!m_origin_item->isSelected());
             }
 
-            // If clicked while holding Alt key, start resizing
+            // ******************* If clicked while holding Alt key, start resizing
             if (event->modifiers() & Qt::KeyboardModifier::AltModifier) {
                 if (scene()->selectedItems().count() > 0) {
                     m_is_resizing = true;
@@ -117,18 +117,18 @@ void SceneGraphicsView::mousePressEvent(QMouseEvent *event)
                         m_selection_rect = m_selection_rect.united(item->sceneBoundingRect());
 
                         // Store starting scale of each selected item
-                        QSizeF my_scale(item->transform().m11(), item->transform().m22());
+                        QPointF my_scale(item->transform().m11(), item->transform().m22());
                         item->setData(User_Roles::Scale, my_scale);
 
                         // Store starting position of each selected item
-                        QSizeF my_pos(item->pos().x(), item->pos().y());
+                        QPointF my_pos(item->pos().x(), item->pos().y());
                         item->setData(User_Roles::Position, my_pos);
                     }
                     return;
                 }
             }
 
-            // If theres no item under mouse, start selection box
+            // ******************* If theres no item under mouse, start selection box
             if (m_origin_item == nullptr) {
                 m_is_selecting = true;                                                  // Flag that we're in selection mode
                 if (event->modifiers() & Qt::KeyboardModifier::ControlModifier) {
@@ -155,7 +155,7 @@ void SceneGraphicsView::mouseMoveEvent(QMouseEvent *event)
 {
     if (selection_mutex.tryLock() == false) return;           // Try and lock function, so we ony run this once at a time
 
-    // If we're in selection mode, process mouse movement and resize box as needed
+    // ******************* If we're in selection mode, process mouse movement and resize box as needed
     if (m_is_selecting) {
         m_rubber_band->setGeometry(QRect(m_origin, event->pos()).normalized());                             // Resize selection box
 
@@ -182,8 +182,7 @@ void SceneGraphicsView::mouseMoveEvent(QMouseEvent *event)
 
     }
 
-
-    // If mouse moved while Alt key is held down, resize
+    // ******************* If mouse moved while Alt key is held down, resize
     if (m_is_resizing) {
         //qreal dist = sqrt(pow(m_click_pos.x() - pos.x(), 2) + pow(m_click_pos.y() - pos.y(), 2));
         QPointF mouse_in_scene =  mapToScene(event->pos());
@@ -202,18 +201,20 @@ void SceneGraphicsView::mouseMoveEvent(QMouseEvent *event)
         for (auto item : scene()->selectedItems()) {
             // Get stored data from each item
             QVariant get_data;
-            QSizeF my_scale, my_pos;
-            get_data = item->data(User_Roles::Scale);       my_scale = get_data.toSizeF();
-            get_data = item->data(User_Roles::Position);    my_pos =   get_data.toSizeF();
+            QPointF my_scale, my_pos;
+            get_data = item->data(User_Roles::Scale);       my_scale = get_data.toPointF();
+            get_data = item->data(User_Roles::Position);    my_pos =   get_data.toPointF();
 
             // Apply new scale to each item
             QTransform scale_transform;
-            scale_transform = QTransform::fromScale(my_scale.width() * scale_x, my_scale.height() * scale_y);
+            scale_transform = QTransform::fromScale(my_scale.x() * scale_x, my_scale.y() * scale_y);
             item->setTransform(scale_transform);
 
             // Move each item as neccessary to keep them aligned within selection box
-            qreal new_left = my_pos.width() * scale_x;
-            qreal new_top =  my_pos.height() * scale_y;
+            qreal original_x_dist = (my_pos.x() - top_left_select.x());
+            qreal original_y_dist = (my_pos.y() - top_left_select.y());
+            qreal new_left = top_left_select.x() + (original_x_dist * scale_x);
+            qreal new_top =  top_left_select.y() + (original_y_dist * scale_y);
             item->setPos(new_left, new_top);
         }
 
@@ -260,51 +261,6 @@ void SceneGraphicsView::mouseReleaseEvent(QMouseEvent *event)
 }
 
 
-
-void SceneGraphicsView::paintEvent(QPaintEvent *event)
-{
-    // Go ahead and paint scene
-    QGraphicsView::paintEvent(event);
-
-    // Draw selection boxes around all selected items
-    QPainter painter(viewport());
-    QBrush pen_brush(m_interface->getColor(Window_Colors::Text_Light));
-    painter.setPen(QPen(pen_brush, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
-    painter.setBrush(Qt::NoBrush);
-
-    for (auto item: scene()->selectedItems()) {
-        QRectF rect = item->boundingRect();                                 // Get item bounding box
-        rect.setWidth(rect.width() * item->transform().m11());              // Transform to item scale
-        rect.setHeight(rect.height() * item->transform().m22());
-        rect.translate(item->pos().x(), item->pos().y());                   // Translate bounding box to proper scene location
-        QPolygon to_view = mapFromScene(rect);                              // Convert scene location to view location
-        painter.drawPolygon(to_view);                                       // Draw polygon
-    }
-}
-
-
-
-// Custom Paint override so we can draw our Rubber Band selection box with custom colors
-void SceneViewRubberBand::paintEvent(QPaintEvent *)
-{
-    QColor bg = m_interface->getColor(Window_Colors::Icon_Light);
-    QStylePainter painter(this);
-
-    QPen pen;
-    pen.setStyle(Qt::PenStyle::SolidLine); //DashLine);
-    pen.setWidth(2);
-    pen.setColor(bg);
-    QBrush brush;
-    brush.setStyle(Qt::BrushStyle::SolidPattern);
-    bg.setAlpha(64);
-    brush.setColor(bg);
-
-    painter.setPen(pen);
-    painter.setBrush(brush);
-    painter.drawRect(this->rect());
-}
-
-
 // Handles zooming in / out of view with mouse wheel
 #if QT_CONFIG(wheelevent)
 void SceneGraphicsView::wheelEvent(QWheelEvent *event)
@@ -338,6 +294,48 @@ void SceneGraphicsView::applyUpdatedMatrix()
 }
 
 
+
+void SceneGraphicsView::paintEvent(QPaintEvent *event)
+{
+    // Go ahead and paint scene
+    QGraphicsView::paintEvent(event);
+
+    // Draw selection boxes around all selected items
+    QPainter painter(viewport());
+    QBrush pen_brush(m_interface->getColor(Window_Colors::Text_Light));
+    painter.setPen(QPen(pen_brush, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+    painter.setBrush(Qt::NoBrush);
+
+    for (auto item: scene()->selectedItems()) {
+        QRectF rect = item->boundingRect();                                 // Get item bounding box
+        rect.setWidth(rect.width() * item->transform().m11());              // Transform to item scale
+        rect.setHeight(rect.height() * item->transform().m22());
+        rect.translate(item->pos().x(), item->pos().y());                   // Translate bounding box to proper scene location
+        QPolygon to_view = mapFromScene(rect);                              // Convert scene location to view location
+        painter.drawPolygon(to_view);                                       // Draw polygon
+    }
+}
+
+
+// Custom Paint override so we can draw our Rubber Band selection box with custom colors
+void SceneViewRubberBand::paintEvent(QPaintEvent *)
+{
+    QColor bg = m_interface->getColor(Window_Colors::Icon_Light);
+    QStylePainter painter(this);
+
+    QPen pen;
+    pen.setStyle(Qt::PenStyle::SolidLine); //DashLine);
+    pen.setWidth(2);
+    pen.setColor(bg);
+    QBrush brush;
+    brush.setStyle(Qt::BrushStyle::SolidPattern);
+    bg.setAlpha(64);
+    brush.setColor(bg);
+
+    painter.setPen(pen);
+    painter.setBrush(brush);
+    painter.drawRect(this->rect());
+}
 
 
 
