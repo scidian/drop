cf94d84d677be9ab571ef5be975af05fe4fba346
Stephens Nunnally <stevinz@gmail.com>
2019-10-14 00:24:28 -0400

    Inspector image widget primed for drag / drop

--- editor/tree_inspector__widgets_image.cpp
+++ editor/tree_inspector__widgets_image.cpp
@@ -30,6 +30,7 @@ QFrame* TreeInspector::createImageFrame(DrProperty *property, QFont &font, QSize
 
     // ***** Store current asset key in widget
     QFrame *image_frame = new QFrame();
+    image_frame->setAcceptDrops(true);
     image_frame->setSizePolicy(size_policy);
     image_frame->setMinimumHeight(frame_height);
     image_frame->setObjectName("assetFrame");
@@ -63,35 +64,55 @@ QFrame* TreeInspector::createImageFrame(DrProperty *property, QFont &font, QSize
 //##    DrFilterInspectorImage Event Filter
 //##
 //####################################################################################
-DrFilterInspectorImage::DrFilterInspectorImage(QObject *parent, IEditorRelay *editor_relay) : QObject(parent), m_editor_relay(editor_relay) {
-
-}
-
+DrFilterInspectorImage::DrFilterInspectorImage(QObject *parent, IEditorRelay *editor_relay) : QObject(parent), m_editor_relay(editor_relay) { }
 
 bool DrFilterInspectorImage::eventFilter(QObject *object, QEvent *event) {
 
-    if (event->type() == QEvent::MouseButtonPress) {
+    //DragEnter = 60,                         // drag moves into widget
+    //DragMove = 61,                          // drag moves in widget
+    //DragLeave = 62,                         // drag leaves or is cancelled
+    //Drop = 63,                              // actual drop
 
+    if (event->type() == QEvent::DragEnter) {
+        Dr::SetLabelText(Label_Names::Label_1, "Drag Enter Event: 60, " + Dr::CurrentTimeAsString());
+        event->accept();
+    }
+
+    if (event->type() == QEvent::DragMove ||
+        event->type() == QEvent::DragLeave ||
+        event->type() == QEvent::Drop) {
+        Dr::SetLabelText(Label_Names::Label_1, "Event: " + QString::number(event->type()) + ", " + Dr::CurrentTimeAsString());
+        event->accept();
     }
 
     // ***** Event Debugging
-    if (event->type() != QEvent::MouseButtonPress &&        //   2
-        event->type() != QEvent::MouseButtonRelease &&      //   3
-        event->type() != QEvent::MouseMove &&               //   5
-        event->type() != QEvent::Enter &&                   //  10
-        event->type() != QEvent::Paint &&                   //  12
-        event->type() != QEvent::WindowActivate &&          //  24
-        event->type() != QEvent::WindowDeactivate &&        //  25
-        event->type() != QEvent::UpdateLater &&             //  78
-        event->type() != QEvent::ToolTip &&                 // 110
-        event->type() != QEvent::HoverEnter &&              // 127
-        event->type() != QEvent::HoverMove &&               // 129
-        event->type() != QEvent::DynamicPropertyChange      // 170
-        ) {
-
-        Dr::SetLabelText(Label_Names::Label_1, "Event: " + QString::number(event->type()) + ", " + Dr::CurrentTimeAsString());
-
-    }
+//    if (event->type() != QEvent::MouseButtonPress &&        //   2
+//        event->type() != QEvent::MouseButtonRelease &&      //   3
+//        event->type() != QEvent::MouseMove &&               //   5
+//        event->type() != QEvent::Enter &&                   //  10
+//        event->type() != QEvent::Leave &&                   //  11
+//        event->type() != QEvent::Paint &&                   //  12
+//        event->type() != QEvent::Move &&                    //  13
+//        event->type() != QEvent::Resize &&                  //  14
+//        event->type() != QEvent::Show &&                    //  17
+//        event->type() != QEvent::Hide &&                    //  18
+//        event->type() != QEvent::WindowActivate &&          //  24
+//        event->type() != QEvent::WindowDeactivate &&        //  25
+//        event->type() != QEvent::ShowToParent &&            //  26
+//        event->type() != QEvent::Wheel &&                   //  31
+//        event->type() != QEvent::PaletteChange &&           //  39
+//        event->type() != QEvent::ChildPolished &&           //  69
+//        event->type() != QEvent::PolishRequest &&           //  74
+//        event->type() != QEvent::LayoutRequest &&           //  76
+//        event->type() != QEvent::UpdateLater &&             //  78
+//        event->type() != QEvent::ToolTip &&                 // 110
+//        event->type() != QEvent::HoverEnter &&              // 127
+//        event->type() != QEvent::HoverLeave &&              // 128
+//        event->type() != QEvent::HoverMove &&               // 129
+//        event->type() != QEvent::DynamicPropertyChange      // 170
+//        ) {
+//        Dr::SetLabelText(Label_Names::Label_1, "Event: " + QString::number(event->type()) + ", " + Dr::CurrentTimeAsString());
+//    }
 
     return QObject::eventFilter(object, event);
 }
