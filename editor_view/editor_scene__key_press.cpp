a5f60c0ac8de451c0f2956606e5821a39e46ce61
Stephens Nunnally <stevinz@gmail.com>
2019-10-18 21:30:07 -0400

    Reoranizing of editor_scene__key_press.cpp

--- editor_view/editor_scene__key_press.cpp
+++ editor_view/editor_scene__key_press.cpp
@@ -49,10 +49,6 @@ void DrScene::keyPressEvent(QKeyEvent *event) {
         return;
     }
 
-    // Find total bounding box of all selected items
-    QRectF source_rect = totalSelectionSceneRect();
-
-
     // ***** Start tracking time key has been pressed
     Qt::Key key_pressed = static_cast<Qt::Key>(event->key());
     if (m_key_down[key_pressed] == false) {
@@ -61,123 +57,145 @@ void DrScene::keyPressEvent(QKeyEvent *event) {
         m_key_down[key_pressed] = true;
     }
 
-    // ***** Amount to move items when arrow keys are pressed
-    QPointF grid_scale = m_current_stage->getComponentPropertyValue(Components::Stage_Grid, Properties::Stage_Grid_Scale).toPointF();
-    QPointF grid_size =  m_current_stage->getComponentPropertyValue(Components::Stage_Grid, Properties::Stage_Grid_Size).toPointF();
-    QPointF move_by (grid_size.x() * grid_scale.x(), grid_size.y() * grid_scale.y());
 
-    if (Dr::GetPreference(Preferences::World_Editor_Snap_To_Grid).toBool() == false) {
-        if        (m_key_timers[key_pressed].elapsed() <  750) {    move_by.setX(   1.0 );  move_by.setY(   1.0 );
-        } else if (m_key_timers[key_pressed].elapsed() < 1500) {    move_by.setX(   5.0 );  move_by.setY(   5.0 );
-        } else if (m_key_timers[key_pressed].elapsed() < 2250) {    move_by.setX(  25.0 );  move_by.setY(  25.0 );
-        } else {                                                    move_by.setX( 100.0 );  move_by.setY( 100.0 ); }
-    }
+    // ********** Process Movement key press on all selected items
+    if (key_pressed == Qt::Key_Up || key_pressed == Qt::Key_Down || key_pressed == Qt::Key_Left || key_pressed == Qt::Key_Right) {
+        QPointF grid_scale = m_current_stage->getComponentPropertyValue(Components::Stage_Grid, Properties::Stage_Grid_Scale).toPointF();
+        QPointF grid_size =  m_current_stage->getComponentPropertyValue(Components::Stage_Grid, Properties::Stage_Grid_Size).toPointF();
 
-    // ********** Process movement key press on all selected items
-    for (auto item : getSelectionItems()) {
-        double move_x = 0.0;
-        double move_y = 0.0;
-        if (key_pressed == Qt::Key_Up)     move_y = -move_by.y();
-        if (key_pressed == Qt::Key_Down)   move_y =  move_by.y();
-        if (key_pressed == Qt::Key_Left)   move_x = -move_by.x();
-        if (key_pressed == Qt::Key_Right)  move_x =  move_by.x();
-        if (Dr::FuzzyCompare(0.0, move_x) == false || Dr::FuzzyCompare(0.0, move_y) == false) {
-            // Store current settings and prepare for ItemChange Event
-            View_Mode before_mode = m_editor_relay->currentViewMode();
-            bool      snap_center = Dr::GetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box).toBool();
-            Dr::SetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box, false);
-            if (m_editor_relay->getStageView()) m_editor_relay->getStageView()->setViewMode(View_Mode::Holding_Keys);
-
-            // Move Item, ItemChange Event is activated for snapping to grid
-            item->moveBy( move_x, move_y);
-
-            // Restore settings after ItemChange Event is over
-            if (m_editor_relay->getStageView()) m_editor_relay->getStageView()->setViewMode(before_mode);
-            Dr::SetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box, snap_center);
+        // Amount to move items when arrow keys are pressed
+        QPointF move_by (grid_size.x() * grid_scale.x(), grid_size.y() * grid_scale.y());
+        if (Dr::GetPreference(Preferences::World_Editor_Snap_To_Grid).toBool() == false) {
+            if        (m_key_timers[key_pressed].elapsed() <  750) {    move_by.setX(   1.0 );  move_by.setY(   1.0 );
+            } else if (m_key_timers[key_pressed].elapsed() < 1500) {    move_by.setX(   5.0 );  move_by.setY(   5.0 );
+            } else if (m_key_timers[key_pressed].elapsed() < 2250) {    move_by.setX(  25.0 );  move_by.setY(  25.0 );
+            } else {                                                    move_by.setX( 100.0 );  move_by.setY( 100.0 ); }
         }
 
-        if (key_pressed == Qt::Key_Comma)  item->setZValue(selectedItems().first()->zValue() - 1);
-        if (key_pressed == Qt::Key_Period) item->setZValue(selectedItems().first()->zValue() + 1);
+        for (auto item : getSelectionItems()) {
+            double move_x = 0.0;
+            double move_y = 0.0;
+            if (key_pressed == Qt::Key_Up)     move_y = -move_by.y();
+            if (key_pressed == Qt::Key_Down)   move_y =  move_by.y();
+            if (key_pressed == Qt::Key_Left)   move_x = -move_by.x();
+            if (key_pressed == Qt::Key_Right)  move_x =  move_by.x();
+            if (Dr::FuzzyCompare(0.0, move_x) == false || Dr::FuzzyCompare(0.0, move_y) == false) {
+                // Store current settings and prepare for ItemChange Event
+                View_Mode before_mode = m_editor_relay->currentViewMode();
+                bool      snap_center = Dr::GetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box).toBool();
+                Dr::SetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box, false);
+                if (m_editor_relay->getStageView()) m_editor_relay->getStageView()->setViewMode(View_Mode::Holding_Keys);
 
+                // Move Item, ItemChange Event is activated for snapping to grid
+                item->moveBy( move_x, move_y);
+
+                // Restore settings after ItemChange Event is over
+                if (m_editor_relay->getStageView()) m_editor_relay->getStageView()->setViewMode(before_mode);
+                Dr::SetPreference(Preferences::World_Editor_Snap_To_Center_Of_Selection_Box, snap_center);
+            }
+        }
+        updateSelectionBox();
     }
-    updateSelectionBox();
 
 
-    // ********** Perform key press event on all items in selection group
-    if (scene_mutex.tryLock(10) == false) return;
+    // ********** Process Layering key press on all selected items
+    if (key_pressed == Qt::Key_Comma || key_pressed == Qt::Key_Period) {
+        QList<long> sorted_keys = m_current_stage->thingKeysSortedByZOrder();
 
-    // ***** Go through any items selected and make copies of them if the copy keys (wasd) were pressed
-    QList<QGraphicsItem*>  list_old_items = getSelectionItems();
-    QList<QGraphicsItem*>  list_new_items { };
+        for (auto item : getSelectionItems()) {
+            if (key_pressed == Qt::Key_Comma) {
+                item->setZValue(selectedItems().first()->zValue() - 1);
+            }
 
-    for (auto item : list_old_items) {
-        DrItem   *dritem   = dynamic_cast<DrItem*>(item);
-        DrThing  *drthing =  dritem->getThing();
-        DrStage  *drstage  = drthing->getParentStage();
-        DrThing  *new_object;
-
-        double  new_x, new_y, new_z;
-
-        switch (key_pressed) {
-            // ***** Clone selected items
-            case Qt::Key::Key_W:
-            case Qt::Key::Key_A:
-            case Qt::Key::Key_S:
-            case Qt::Key::Key_D:
-                new_x = drthing->getComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position).toPointF().x();
-                new_y = drthing->getComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position).toPointF().y();
-                new_z = drthing->getComponentPropertyValue(Components::Thing_Layering,  Properties::Thing_Z_Order).toDouble();
-
-                if (key_pressed == Qt::Key::Key_W) new_y = new_y - source_rect.height();
-                if (key_pressed == Qt::Key::Key_A) new_x = new_x - source_rect.width();
-                if (key_pressed == Qt::Key::Key_S) new_y = new_y + source_rect.height();
-                if (key_pressed == Qt::Key::Key_D) new_x = new_x + source_rect.width();
-
-                new_object = drstage->addThing(drthing->getThingType(), drthing->getAssetKey(), new_x, new_y, new_z);
-                drstage->copyThingSettings(drthing, new_object);
-                new_object->setComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position, QPointF(new_x, new_y));
-                new_object->setComponentPropertyValue(Components::Thing_Layering,  Properties::Thing_Z_Order, new_z);
-
-                list_new_items.append( this->addItemToSceneFromThing(new_object) );
-                break;
-
-            // ***** Delete selected items
-            case Qt::Key::Key_Delete:
-            case Qt::Key::Key_Backspace:
-                blockSignals(true);
-                drstage->deleteThing(drthing);
-                list_old_items.removeOne(item);
-                removeItem( item );
-                delete item;
-                update_widgets_when_done = true;
-                blockSignals(false);
-                break;
-            default: ;
+            if (key_pressed == Qt::Key_Period) {
+                item->setZValue(selectedItems().first()->zValue() + 1);
+            }
         }
     }
 
-    // ***** If we added (copied) new items to scene, select those items
-    if (list_new_items.count() > 0 || list_old_items.count() == 0) {
-        blockSignals(true);
-        this->clearSelection();
-        for (auto item : list_new_items) {
-            item->setSelected(true);
+
+    // ********** Process Duplicate and Delete key press on all selected items
+    if (key_pressed == Qt::Key_W || key_pressed == Qt::Key_A || key_pressed == Qt::Key_S || key_pressed == Qt::Key_D ||
+        key_pressed == Qt::Key_Delete ||
+        key_pressed == Qt::Key_Backspace    ) {
+
+        // ***** Lock Scene Mutex
+        if (scene_mutex.tryLock(10) == false) return;
+
+        QRectF source_rect =  totalSelectionSceneRect();
+        QList<QGraphicsItem*> list_old_items = getSelectionItems();
+        QList<QGraphicsItem*> list_new_items { };
+
+        for (auto item : list_old_items) {
+            DrItem   *dritem   = dynamic_cast<DrItem*>(item);
+            DrThing  *drthing =  dritem->getThing();
+            DrStage  *drstage  = drthing->getParentStage();
+            DrThing  *new_object;
+
+            double  new_x, new_y, new_z;
+
+            switch (key_pressed) {
+                // ***** Clone selected items
+                case Qt::Key::Key_W:
+                case Qt::Key::Key_A:
+                case Qt::Key::Key_S:
+                case Qt::Key::Key_D:
+                    new_x = drthing->getComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position).toPointF().x();
+                    new_y = drthing->getComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position).toPointF().y();
+                    new_z = drthing->getComponentPropertyValue(Components::Thing_Layering,  Properties::Thing_Z_Order).toDouble();
+
+                    if (key_pressed == Qt::Key::Key_W) new_y = new_y - source_rect.height();
+                    if (key_pressed == Qt::Key::Key_A) new_x = new_x - source_rect.width();
+                    if (key_pressed == Qt::Key::Key_S) new_y = new_y + source_rect.height();
+                    if (key_pressed == Qt::Key::Key_D) new_x = new_x + source_rect.width();
+
+                    new_object = drstage->addThing(drthing->getThingType(), drthing->getAssetKey(), new_x, new_y, new_z);
+                    drstage->copyThingSettings(drthing, new_object);
+                    new_object->setComponentPropertyValue(Components::Thing_Transform, Properties::Thing_Position, QPointF(new_x, new_y));
+                    new_object->setComponentPropertyValue(Components::Thing_Layering,  Properties::Thing_Z_Order, new_z);
+
+                    list_new_items.append( this->addItemToSceneFromThing(new_object) );
+                    break;
+
+                // ***** Delete selected items
+                case Qt::Key::Key_Delete:
+                case Qt::Key::Key_Backspace:
+                    blockSignals(true);
+                    drstage->deleteThing(drthing);
+                    list_old_items.removeOne(item);
+                    removeItem( item );
+                    delete item;
+                    update_widgets_when_done = true;
+                    blockSignals(false);
+                    break;
+                default: ;
+            }
         }
-        update_widgets_when_done = true;
-        blockSignals(false);
-        selectionChanged();
+
+        // ***** If we added (copied) new items to scene, select those items
+        if (list_new_items.count() > 0 || list_old_items.count() == 0) {
+            blockSignals(true);
+            this->clearSelection();
+            for (auto item : list_new_items) {
+                item->setSelected(true);
+            }
+            update_widgets_when_done = true;
+            blockSignals(false);
+            selectionChanged();
+        }
+
+        // ***** Unlock Scene Mutex
+        scene_mutex.unlock();
     }
 
-    // ***** Unlock Scene Mutex
-    scene_mutex.unlock();
 
-    // ***** Update Editor Widgets
+    // ********** Update Editor Widgets
     if (update_widgets_when_done) {
         m_editor_relay->buildProjectTree();
         m_editor_relay->updateItemSelection(Editor_Widgets::Stage_View);
     }
 
-    // !!!!! #NOTE: Don't pass on, if we pass on arrow key presses, it moves view sliders
+    // ********** #NOTE: Don't pass on, if we pass on arrow key presses, it moves view sliders
     ///QGraphicsScene::keyPressEvent(event);
 }
 
